3d1c5a55f09d58b44c1af196da179def92bb6716
masa-pc <153816767+masa-pc@users.noreply.github.com>
2024-07-07 22:30:58 +0900

    Add files via upload

--- tax.js
+++ tax.js
@@ -1,12 +1,61 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
-const h1 = document.getElementById("h1")
+const h1 = document.getElementById("h1");
+const taxSwitch = document.getElementById("taxSwitch");
+let tax_switch = 0;
+let tax_switch_check = 0;
+tax_check_display(2);
+taxSwitch.innerText = "8%";
+
+let tax_Judgment = 1.08;
+//トグルスイッチ
+console.log(tax_check(tax_switch));
+
+function tax_check_display(num){
+    let n = num % 2
+    if(n == 0){
+        check = "8%";
+    }else{
+        check = "10%";
+    }
+    return check
+}
+
+function tax_check(num1){
+    let n = num1 % 2
+    if(n == 0){
+        check1 = 0;
+    }else{
+        check1 = 1;
+    }
+    return check1
+}
+
+const switchOuter = document.querySelector(".switch_outer");
+const toggleSwitch = document.querySelector(".toggle_switch");
+
+switchOuter.addEventListener("click", () =>{
+    switchOuter.classList.toggle("active");
+    toggleSwitch.classList.toggle("active");
+    tax_switch++
+    console.log(tax_switch);
+    let taxSituation = tax_check_display(tax_switch)
+    console.log(taxSituation);
+    taxSwitch.innerText = taxSituation;
+    tax_switch_check = tax_check(tax_switch);
+    console.log(tax_switch_check);
+    input.value = "";
+})
 
 function tax(price) {
-    before_tax_pricce = price / 1.1
+    if(tax_switch_check == 0){
+        tax_Judgment = 1.08;
+    }else{
+        tax_Judgment = 1.1;
+    }
+    before_tax_pricce = price / tax_Judgment
     tax_pricce = Math.round(before_tax_pricce)
-    console.log("hello");
     return tax_pricce;
 }
 
@@ -33,6 +82,8 @@ function add() {
         event.preventDefault();
         input.value = "";
     })
-        
+    
     }
 }
+
+
